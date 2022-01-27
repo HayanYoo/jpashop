f708c5b4fa86f0865fabd826d24b36698746d6f3
HayanYoo <hayanyoo.dev@gmail.com>
2022-01-27 15:25:15 +0900

    feat:엔티티 직접 노출 방법(하지마라)

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -9,5 +11,17 @@ public class JpashopApplication {
     public static void main(String[] args) {
         SpringApplication.run(JpashopApplication.class, args);
     }
+
+
+        // mappedby 된 엔티티의 무한루프를 피하기 위해 JsonIgnore을 붙여 준 뒤 발생하는 프록시 에러 해결 방법
+        // gradle 설치 후 빈 설정
+        // 사실상 엔티티를 외부로 그대로 노출하면 안되기 때문에 이방법은 사용할 필요 없음
+
+        @Bean
+        Hibernate5Module hibernate5Module() {
+            return new Hibernate5Module();
+        }
+
+
 
 }
--- src/main/java/jpabook/jpashop/controller/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/controller/api/OrderSimpleApiController.java
@@ -1,2 +1,33 @@
-package jpabook.jpashop.controller.api;public class OrderSimpleApiController {
+package jpabook.jpashop.controller.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.item.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * order -> Member
+ * order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
 }
