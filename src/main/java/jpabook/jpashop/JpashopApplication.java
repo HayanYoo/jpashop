d1fbf6d29b3c4c0cc37154b78e4fb7419c765da0
HayanYoo <hayanyoo.dev@gmail.com>
2022-03-22 11:28:55 +0900

    jenkins test

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,15 +1,21 @@
 package jpabook.jpashop;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class JpashopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpashopApplication.class, args);
+        log.info("SUCCESS");
+
     }
 
 
